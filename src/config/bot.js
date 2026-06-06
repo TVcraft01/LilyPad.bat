b706aa210e2106f862d7e33daf1286e43fc0713f
TVcraft01 <tvcraft06@gmail.com>
2026-06-06 06:32:48 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Made with electronics",
         // Activity type number (0 = Playing).
         type: 0, 
       },
@@ -88,7 +88,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
+      primary: "#27EBF5", 
       secondary: "#2F3136", 
 
       // Standard status colors for success/error/warning/info messages.
